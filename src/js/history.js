3d4bcfb6e3c06dd33cc46ee55815a3dd01c59a94
p0ulpe <delanoe.william@gmail.com>
2026-03-17 22:47:58 +0100

    fix: prevent history items from overflowing on mobile with many targets

--- src/js/history.js
+++ src/js/history.js
@@ -221,8 +221,10 @@ function createHistoryItemHTML(e) {
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
     </button>
   </div>
-  ${summaryHTML}
-  ${bodyHTML}
+  <div class="hist-scroll-body">
+    ${summaryHTML}
+    ${bodyHTML}
+  </div>
 </div>`;
 }
 
